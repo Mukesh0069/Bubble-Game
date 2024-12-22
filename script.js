5b6ec902b2595fd5e0e72da806a533207375abf3
man-from-mars <mukeshrangatand123@gmail.com>
2024-12-22 14:25:42 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+var score = 0;
+var timer = 60;
+var randomNumber = 0;
+var callCount = 0;
+var hitvalue = () =>{
+    randomNumber = Math.floor(Math.random()*10)
+    document.querySelector("#hitValue").textContent = randomNumber;
+}
+var makeBubble = () =>{
+    var clutter = "";
+    for(var i = 0; i <= 492; i++){
+        var randomNum = Math.floor(Math.random()*10)
+        clutter += `<div class="item">${randomNum}</div>`;
+    }
+    
+    document.querySelector(".area").innerHTML = clutter;
+    
+}
+
+var runtimer = () =>{
+    var timerINterval = setInterval( () => {
+        if(timer > 0){
+            timer--;
+            document.querySelector("#timer").textContent = timer;
+        }
+        else{
+            clearInterval(timerINterval);
+            document.querySelector(".area").innerHTML = `<h1>Game Over <br>You Scored ${score}</h1>`;
+
+        }
+    }, 1000);
+}
+
+var calculateScore = () =>{
+    score += 10;
+    document.querySelector("#Score").textContent = score;
+}
+var deductScore = () =>{
+    callCount++;
+    score -= 10;
+    document.querySelector("#Score").textContent = score;
+}
+
+makeBubble();
+runtimer();
+hitvalue();
+
+var bubbleContainer = document.querySelector("#bubbleContainer");
+    bubbleContainer.addEventListener("click", (details) =>{
+        var clickedValue = Number(details.target.textContent);
+        if(clickedValue === randomNumber){
+            calculateScore();
+            makeBubble();
+            hitvalue();
+        }
+        else if(callCount > 2){
+            document.querySelector(".area").innerHTML = `<h1>You Lost <br>You Scored ${score}</h1>`;
+            
+        }
+        else{
+            deductScore();
+            hitvalue();
+            makeBubble();
+           }
+        
+    })
